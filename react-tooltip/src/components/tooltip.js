87068318bb168201e2d759a8ade097e9c947b17d
Ben Pyrik <ben@wsundine.com>
2019-11-03 15:34:19 -0600

    Hookify react-tooltip example

--- react-tooltip/src/components/tooltip.js
+++ react-tooltip/src/components/tooltip.js
@@ -1,31 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-export default class Tooltip extends React.Component {
+export default function Tooltip(props) {
+  const { features } = props;
 
-  static propTypes = {
-    features: PropTypes.array.isRequired
-  };
-
-  render() {
-    const { features } = this.props;
-
-    const renderFeature = (feature, i) => {
-      return (
-        <div key={i}>
-          <strong className='mr3'>{feature.layer['source-layer']}:</strong>
-          <span className='color-gray-light'>{feature.layer.id}</span>
-        </div>
-      )
-    };
-
+  const renderFeature = (feature, i) => {
     return (
-      <div className="flex-parent-inline flex-parent--center-cross flex-parent--column absolute bottom">
-        <div className="flex-child px12 py12 bg-gray-dark color-white shadow-darken10 round txt-s w240 clip txt-truncate">
-          {features.map(renderFeature)}
-        </div>
-        <span className="flex-child color-gray-dark triangle triangle--d"></span>
+      <div key={i}>
+        <strong className="mr3">{feature.layer["source-layer"]}:</strong>
+        <span className="color-gray-light">{feature.layer.id}</span>
       </div>
     );
-  }
+  };
+
+  return (
+    <div className="flex-parent-inline flex-parent--center-cross flex-parent--column absolute bottom">
+      <div className="flex-child px12 py12 bg-gray-dark color-white shadow-darken10 round txt-s w240 clip txt-truncate">
+        {features.map(renderFeature)}
+      </div>
+      <span className="flex-child color-gray-dark triangle triangle--d"></span>
+    </div>
+  );
 }
+
+Tooltip.propTypes = {
+  features: PropTypes.array.isRequired
+};
--- react-tooltip/src/index.js
+++ react-tooltip/src/index.js
@@ -1,57 +1,59 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import mapboxgl from 'mapbox-gl'
-import Tooltip from './components/tooltip'
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
+import Tooltip from "./components/tooltip";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
 
-class Application extends React.Component {
-  tooltipContainer;
+function Application() {
+  const mapContainer = useRef(null);
+  // Container to put React generated content in.
+  const tooltipContainer = useRef(document.createElement("div"));
 
-  setTooltip(features) {
+  const setTooltip = features => {
     if (features.length) {
       ReactDOM.render(
-        React.createElement(
-          Tooltip, {
-            features
-          }
-        ),
-        this.tooltipContainer
+        React.createElement(Tooltip, {
+          features
+        }),
+        tooltipContainer.current
       );
     } else {
-      ReactDOM.unmountComponentAtNode(this.tooltipContainer);
+      ReactDOM.unmountComponentAtNode(tooltipContainer.current);
     }
-  }
+  };
 
-  componentDidMount() {
-
-    // Container to put React generated content in.
-    this.tooltipContainer = document.createElement('div');
-
+  // Initialization.
+  useEffect(() => {
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v9",
       center: [-79.38, 43.65],
       zoom: 12.5
     });
 
-    const tooltip = new mapboxgl.Marker(this.tooltipContainer, {
-      offset: [-120, 0]
-    }).setLngLat([0,0]).addTo(map);
+    map.on("load", () => {
+      const tooltip = new mapboxgl.Marker(tooltipContainer.current, {
+        offset: [-120, 0]
+      })
+        .setLngLat([0, 0])
+        .addTo(map);
 
-    map.on('mousemove', (e) => {
-      const features = map.queryRenderedFeatures(e.point);
-      tooltip.setLngLat(e.lngLat);
-      map.getCanvas().style.cursor = features.length ? 'pointer' : '';
-      this.setTooltip(features);
+      map.on("mousemove", e => {
+        const features = map.queryRenderedFeatures(e.point);
+        tooltip.setLngLat(e.lngLat);
+        map.getCanvas().style.cursor = features.length ? "pointer" : "";
+        setTooltip(features);
+      });
     });
-  }
 
-  render() {
-    return (
-      <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
-    );
-  }
+    return function cleanup() {
+      map.remove();
+    };
+  }, []);
+
+  return <div ref={mapContainer} className="absolute top right left bottom" />;
 }
 
-ReactDOM.render(<Application />, document.getElementById('app'));
+ReactDOM.render(<Application />, document.getElementById("app"));
