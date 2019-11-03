c55431d5e6b0d68cf6c72c4546c33985e393450b
Ben Pyrik <ben@wsundine.com>
2019-11-03 13:53:45 -0600

    Hookify basic example

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,53 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import mapboxgl from 'mapbox-gl'
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import mapboxgl from "mapbox-gl";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
 
-class Application extends React.Component {
+function Application() {
+  const [latLngZoom, setLatLngZoom] = useState({ lng: 5, lat: 34, zoom: 1.5 });
+  const mapContainer = useRef(null);
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 1.5
-    };
-  }
-
-  componentDidMount() {
-    const { lng, lat, zoom } = this.state;
-
+  // Initialization.
+  useEffect(() => {
     const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9',
-      center: [lng, lat],
-      zoom
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v9",
+      center: [5, 34],
+      zoom: 1.5
     });
 
-    map.on('move', () => {
+    map.on("move", () => {
       const { lng, lat } = map.getCenter();
-
-      this.setState({
+      setLatLngZoom({
         lng: lng.toFixed(4),
         lat: lat.toFixed(4),
         zoom: map.getZoom().toFixed(2)
       });
     });
-  }
 
-  render() {
-    const { lng, lat, zoom } = this.state;
+    return function cleanup() {
+      map.remove();
+    };
+  }, []);
 
-    return (
-      <div>
-        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
-        </div>
-        <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+  const { lng, lat, zoom } = latLngZoom;
+  return (
+    <>
+      <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+        <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
       </div>
-    );
-  }
+      <div ref={mapContainer} className="absolute top right left bottom" />
+    </>
+  );
 }
 
-ReactDOM.render(<Application />, document.getElementById('app'));
+ReactDOM.render(<Application />, document.getElementById("app"));
